9a5765a0080066dc801100815f3d6cb3ae6f44f3
Marc Romo Tarre <marcromotarre@MacBook-Pro-de-Marc.local>
2023-06-22 01:02:59 +0200

    my-components

--- src/data/ComponentsData.tsx
+++ src/data/ComponentsData.tsx
@@ -1,5 +1,15 @@
 export default [
   {
+    name: "<Button />",
+    backgoundColor: "bg-denim",
+    url: "/react/my-components/button/use",
+  },
+  {
+    name: "<Alert />",
+    backgoundColor: "bg-denim",
+    url: "/react/my-components/alert",
+  },
+  {
     name: "<Modal />",
     backgoundColor: "bg-denim",
     url: "/react/my-components/modal",
