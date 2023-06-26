06eb82b4b129af6840f65713db599e55810a02ec
Marc Romo Tarre <marcromotarre@MacBook-Pro-de-Marc.local>
2023-06-26 11:34:29 +0200

    card

---
+++ src/pages/react/my-components/ReactMyComponentsCardPage.tsx
@@ -0,0 +1,20 @@
+import Typography from "../../../components/common/Typography/Typography";
+import Card from "../../../components/common/card/Card";
+
+const ReactMyComponentsCardPage = () => {
+  return (
+    <>
+      <Card>
+        <Typography>This is a simple Card</Typography>
+      </Card>
+      <Card type="type2">
+        <Typography>This is a simple Card</Typography>
+      </Card>
+      <Card type="type3">
+        <Typography>This is a simple Card</Typography>
+      </Card>
+    </>
+  );
+};
+
+export default ReactMyComponentsCardPage;
