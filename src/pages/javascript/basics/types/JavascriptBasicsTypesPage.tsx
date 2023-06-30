fcd036686389033c696090abd7bd94d4be65eccf
Marc Romo Tarre <marcromotarre@MacBook-Pro-de-Marc.local>
2023-06-30 02:45:26 +0200

    types

--- src/pages/javascript/basics/types/JavascriptBasicsTypesPage.tsx
+++ src/pages/javascript/basics/types/JavascriptBasicsTypesPage.tsx
@@ -1,5 +1,9 @@
 import Alert from "../../../../components/common/alert/Alert";
 import Code from "../../../../components/common/code/Code";
+import QuestionAnswer, {
+  Answer,
+  Question,
+} from "../../../../components/common/question-answer/QuestionAnswer";
 import Typography from "../../../../components/common/typography/Typography";
 
 const JavascriptBasicsTypesPage = () => {
@@ -206,10 +210,115 @@ function sign(v) {
 sign(-3)          // -1
 sign(3)           // 1
 sign(-0)          // -1
-sign(0)           // 1
-
-`}</>
+sign(0)           // 1`}</>
       </Code>
+
+      <Typography variant="h5">Fundamental Objects</Typography>
+      <Typography>When we should use new?</Typography>
+      <Alert severity="success">
+        {`Use new: Object(), Array(), Function(), Date(), RegExp(), Error()`}
+      </Alert>
+      <Alert severity="error">
+        {`Do not use new: String(), Number(), Boolean()`}
+      </Alert>
+      <Code noHeader>
+        <>{`
+var yesterday = new Date("May 19 2023");
+yesterday.toUTCString();
+// 'Thu, 18 May 2023 22:00:00 GMT'
+
+var myGPA = String(transcript.gpa)`}</>
+      </Code>
+      <Typography variant="h5">Exercise:</Typography>
+      <Code noHeader>
+        <>{`
+# Polyfill for 'Object.is(..)'
+
+In this exercise, you will define a polyfill for 'Object.is(..)'. No cheating and looking it up online!
+
+## Instructions
+
+1. 'Object.is(..)' should take two parameters.
+
+2. It should return 'true' if the passed in parameters are exactly the same value (not just '===' -- see below!), or 'false' otherwise.
+
+3. For 'NaN' testing, you can use 'Number.isNaN(..)', but first see if you can find a way to test without usage of any utility?
+
+4. For '-0' testing, no built-in utility exists, but here's a hint: '-Infinity'.
+
+5. If the parameters are any other values, just test them for strict equality.
+
+6. You cannot use the built-in 'Object.is(..)' -- that's cheating!
+
+## Polyfill Pattern
+
+**NOTE:** Since your JS environment probably already has 'Object.is(..)', to test your polyfill you'll have to first unconditionally define it (no 'if' guard), and then add the 'if' guard when you're done.
+
+To define a polyfill, it looks like this:
+
+'''js
+if (!Object.is) {
+	Object.is = function ObjectIs(..) { .. };
+}`}</>
+      </Code>
+      <Typography>
+        To know if you resolve it correctly you can pass these tests
+      </Typography>
+      <Code noHeader>
+        <>{`
+// tests:
+console.log(Object.is(42,42) === true);
+console.log(Object.is("foo","foo") === true);
+console.log(Object.is(false,false) === true);
+console.log(Object.is(null,null) === true);
+console.log(Object.is(undefined,undefined) === true);
+console.log(Object.is(NaN,NaN) === true);
+console.log(Object.is(-0,-0) === true);
+console.log(Object.is(0,0) === true);
+
+console.log(Object.is(-0,0) === false);
+console.log(Object.is(0,-0) === false);
+console.log(Object.is(0,NaN) === false);
+console.log(Object.is(NaN,0) === false);
+console.log(Object.is(42,"42") === false);
+console.log(Object.is("42",42) === false);
+console.log(Object.is("foo","bar") === false);
+console.log(Object.is(false,true) === false);
+console.log(Object.is(null,undefined) === false);
+console.log(Object.is(undefined,null) === false);`}</>
+      </Code>
+      <QuestionAnswer>
+        <Question>Solution</Question>
+        <Answer>
+          <Code noHeader>
+            <>{`
+if(!Object.is || true) {
+  Object.is = function ObjectIs(x, y) {
+    var xNegZero = isItNegZero(x);
+    var yNegZero = isItNegZero(y);
+
+    if(xNegZero || yNegZero) {
+      return xNegZero && yNegZero
+    }
+    else if (isItNaN(x) && isItNaN(y)) {
+      return true
+    }
+    else {
+      return x === y;
+    }
+
+    function isItNegZero(v) {
+      return v === 0 && (1/v) == -Infinity;
+    }
+
+    function isItNaN(v) {
+      return v !== v;
+    }
+  }
+}`}</>
+          </Code>
+        </Answer>
+      </QuestionAnswer>
     </>
   );
 };
