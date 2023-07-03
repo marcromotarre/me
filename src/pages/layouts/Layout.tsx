918fe5b6a6750198b45b981d4743bb9111ca86f2
Marc Romo Tarre <marcromotarre@MacBook-Pro-de-Marc.local>
2023-07-04 01:15:51 +0200

    remove header

--- src/pages/layouts/Layout.tsx
+++ src/pages/layouts/Layout.tsx
@@ -94,9 +94,6 @@ const InformationLayout = () => {
     );
   return (
     <div className="flex h-[100vh] flex-col">
-      <div className="h-[50px] flex-initial">
-        <Header />
-      </div>
       <div className="grid h-[calc(100%_-_50px)] sm:grid-cols-[0px_auto] md:grid-cols-[0px_auto] lg:grid-cols-[300px_auto]">
         <div className="h-[100%] overflow-x-hidden overflow-y-scroll shadow-2xl sm:invisible md:invisible lg:visible">
           <LateralMenu />
