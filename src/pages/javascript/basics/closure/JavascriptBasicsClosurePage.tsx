f81c9099cacd361bfe85eec490d3206d69a24ee3
Marc Romo Tarre <marcromotarre@MacBook-Pro-de-Marc.local>
2023-07-04 15:58:15 +0200

    objects

--- src/pages/javascript/basics/closure/JavascriptBasicsClosurePage.tsx
+++ src/pages/javascript/basics/closure/JavascriptBasicsClosurePage.tsx
@@ -19,6 +19,7 @@ const JavascriptBasicsClosurePage = () => {
         it&apos;s able to access that lexical scope, even when that function
         executes in a different scope.
       </Alert>
+      <Typography>Here you have a couple of examples:</Typography>
       <Code noHeader>
         <>{`
 function outerFunction() {
@@ -45,6 +46,150 @@ function ask() {
 ask("What is closure?);
 // What is closure?
 `}</>
+      </Code>
+      <Typography>
+        See this example here it is using the same variable:
+      </Typography>
+
+      <Code noHeader>
+        <>{`
+for (var i = 1; i <= 3; i++) {
+  setTimeout(function() {
+    console.log("i: " + i);
+
+  }, i * 1000);
+}
+// i: 4
+// i: 4
+// i: 4`}</>
+      </Code>
+      <Typography>
+        So we&apos;re expecting it to print out i 1, i 2, i 3, but when we run
+        it it prints out 4, 4, 4. Why? Cuz there&apos;s only one i variable.
+        There&apos;s an i variable on line 1 and there&apos;s only one of them,
+        and we have three functions. If we wanted to have three separate values,
+        how many variables would we need? 3!
+      </Typography>
+      <Typography>
+        We can&apos;t effectively use closure until we separate ourselves from
+        that perception of capturing values. It&apos;s not capturing a value,
+        preserving access to a variable, okay? So, if we wanted to solve this
+        problem, and we analyzed that, I need separate variables, we&apos;ve
+        seen several multiple different examples of how to create variables. If
+        we wanna create more than one variable with the same name, we need to
+        make new scopes, right? So we could do an iffy here, but the much more
+        common way now that we have ES six, now that we have blockscoping, is to
+        just stick a block scoped declaration in the iteration.
+      </Typography>
+      <Code noHeader>
+        <>{`
+for (var i = 1; i <= 3; i++) {
+  let j = i;
+  setTimeout(function() {
+    console.log("j: " + j);
+
+  }, j * 1000);
+}
+// j: 1
+// j: 2
+// j: 3`}</>
+      </Code>
+      <Typography>There is one more better solution:</Typography>
+      <Code noHeader>
+        <>{`
+for (let i = 1; i <= 3; i++) {
+  setTimeout(function() {
+    console.log("i: " + i);
+
+  }, i * 1000);
+}
+// i: 1
+// i: 2
+// i: 3`}</>
+      </Code>
+      <Typography>
+        Which means the closure just magically works, okay? The point is if you
+        need multiple different values being closed over you need multiple
+        different variables. You need to close over different variables, not try
+        to capture values.
+      </Typography>
+      <Typography variant="h4">Module Pattern</Typography>
+      <Typography>
+        Now, we finally get to talk about the module pattern. We have built
+        ourselves up through a solid understanding of lexical scope. Lexical
+        scope gives us an understanding of how closure works, and closure then
+        is a necessary component to understand the module pattern. Before we
+        look at what the module pattern is, we need to look at what the module
+        pattern is not. This is an extremely common pattern, a common pattern
+        where you have a set of behavior-like functions and a set of data that
+        those functions operate on. And you wanna collect them together into
+        some logical unit, the simplest way is to just make an object and put
+        your data and your functions directly on the object.
+      </Typography>
+      <Typography>
+        I would say that this has a name, unofficially, but this had a name as a
+        pattern, this is the namespace pattern. Taking a set of functions and
+        data and putting them inside of an object, putting them as properties
+        instead of variables, it is effectively collecting them into a
+        namespace. Not really a syntactic feature of the language, but it is an
+        idiom that we make namespaces with objects. And for years, this was an
+        extremely common pattern. This is how people collected functionality
+        together. This was sort of the primitive data structure, if you will.
+        Nothing wrong with this particular pattern.
+      </Typography>
+      <Code noHeader>
+        <>{`
+var workshop = {
+  teacher: "Marc",
+  ask(question) {
+    console.log(this.teacher, question);
+  },
+}
+
+workshop.ask("Is this a module?")
+// Marc Is this a module?`}</>
+      </Code>
+      <Alert severity="info">
+        Modules encapsulate data and behavior (methods) together. The state
+        (data) of a module is held by its methods via closure.
+      </Alert>
+      <Typography>
+        You can not have a module without closure. Minimally expose only
+        what&apos;s necessary. That&apos;s the module pattern.
+      </Typography>
+      <Typography variant="h4">ES6 Modules</Typography>
+      <Code noHeader>
+        <>{`
+var teacher = "Marc";
+
+export default function ask(question) {
+  console.log(teacher, question);
+}`}</>
+      </Code>
+      <Typography>
+        Wrapped in a scope that is by default private. The way you make
+        something public is you use the export keyword, like you see on line 3.
+        So anything you export is public, everything that you don&apos;t export
+        is private. The other thing to know about modules is that they are
+        file-based. Which means it is impossible to have more than one ES6
+        module in the same file. If your application is composed of say, 1,000
+        modules, the way many of your applications are, and you write all of
+        them in ES6 modules, you&apos;ll be writing them all as separate files,
+        which is fine.
+      </Typography>
+
+      <Code noHeader>
+        <>{`
+import ask from "workshop.mjs"
+ask("It is a default import, right?")
+// Marc It is a default import, right?`}</>
+      </Code>
+
+      <Code noHeader>
+        <>{`
+import * as workshop from "workshop.mjs"
+workshop.ask("It is a namespace import, right?")
+// Marc It is a namespace import, right?`}</>
       </Code>
     </>
   );
--- src/pages/javascript/basics/objects/JavascriptBasicsObjectsPage.tsx
+++ src/pages/javascript/basics/objects/JavascriptBasicsObjectsPage.tsx
@@ -1,5 +1,264 @@
+import Alert from "../../../../components/common/alert/Alert";
+import Code from "../../../../components/common/code/Code";
+import Typography from "../../../../components/common/typography/Typography";
+
 const JavascriptBasicsObjectsPage = () => {
-  return <></>;
+  return (
+    <>
+      <Typography>
+        The objects, the this keyword and the prototypes, those make up the
+        objects oriented system. I&apos;m deliberately saying objects oriented
+        instead of object oriented because this is not strictly a class system,
+        there is classes that have been layered on top of it. And that is an
+        option that many people prefer, but it is not inherently a class system.
+        So we&apos;re gonna start by understanding this keyword, which is one of
+        the foundational pieces here.
+      </Typography>
+      <Typography>
+        We&apos;ll look at what the class system does, the class keyword layered
+        on top of JavaScript. And then we&apos;ll circle back to understand
+        where that is syntactic sugar on top of, which is actually the prototype
+        system. And then after we&apos;ve fully understood how JavaScript&apos;s
+        objects system has been used to implement the class pattern, what
+        I&apos;m gonna leave you with at the end of this unit is a discussion
+        about the inheritance pattern from classes as opposed to the delegation
+        pattern. And I&apos;m gonna argue that the delegation pattern is a more
+        powerful pattern that we basically aren&apos;t taking advantage of
+        because we&apos;re focused on only doing classes with JavaScript&apos;s
+        prototypes. The delegation pattern is much more powerful and much more
+        widespread, but we sort of focus very myopically only on classes.
+      </Typography>
+      <Typography>
+        And that leads us to a comparison of coding styles where OO is class
+        oriented in JavaScript, and I&apos;ve come up with a different style of
+        code that I call OLOO, which stands for objects linked to other objects.
+      </Typography>
+      <Typography variant="h4">this keyword</Typography>
+      <Alert severity="info">
+        A function&apos;s this references the execution context for that call,
+        determined entirely by how the function was called.
+      </Alert>
+      <Typography>
+        Which is the counterintuitive part because most people think that you
+        could look at a function, and figure out what this keyword is gonna
+        point out. But the definition of the function does not matter at all, to
+        determining the this keyword. The only thing that matters is how does
+        that function get invoked?
+      </Typography>
+      <Code noHeader>
+        <>{`
+var teacher = "Marc";
+
+function ask(question) {
+  console.log(teacher, question);
+}
+
+function otherClass() {
+  var teacher = "Suzy";
+  ask("Why?");
+}
+
+otherClass();`}</>
+      </Code>
+      <Typography>
+        f that sounds familiar to you, it&apos;s because we&apos;ve sort of
+        already talked about a flexible reusable context. It was what we talk
+        about when we talk about dynamic scoping, and the scope you need of our
+        discussion. So in other words, that these keyword is JavaScript&apos;s
+        version of dynamic scope if it&apos;s this way of having as flexible,
+        reusable behavior.
+      </Typography>
+      <Code noHeader>
+        <>{`
+function ask(question) {
+  console.log(this.teacher, question);
+}
+
+function otherClass() {
+  var myContext = {
+    teacher: "Suzy"
+  };
+  ask.call(myContent, "Why?"); // Suzy Why?
+}
+
+otherClass();`}</>
+      </Code>
+      <Typography>
+        So this keyword in this particular case, will end up pointing at my
+        context. So you see that sort of dynamic flexibility happening here. You
+        see that I could call that same ask function, lots of different ways.
+        And provide lots of different context objects for the this keyword point
+        on, that&apos;s the dynamic flexible reusability of the this keyword..
+        That&apos;s why it exists by the way, it exists so that we can invoke
+        functions in these different contexts.
+      </Typography>
+      <Typography variant="h4">Implicit Binding</Typography>
+      <Code noHeader>
+        <>{`
+var workshop = {
+  teacher: "Marc",
+  ask(question) {
+    console.log(this.teacher, question);
+  }
+};
+
+workshop.ask("What is the implicit binding?");
+// Marc What is the implicit binding?`}</>
+      </Code>
+
+      <Code noHeader>
+        <>{`
+function ask(question) {
+  console.log(this.teacher, question);
+}
+
+var workshop1 = {
+  teacher: "Marc",
+  ask: ask,
+};
+
+var workshop2 = {
+  teacher: "Suzy",
+  ask: ask,
+};
+
+workshop1.ask("How do I share a method?");
+// Marc How do I share a method?
+
+workshop2.ask("How do I share a method?");
+// Suzy How do I share a method?`}</>
+      </Code>
+      <Typography variant="h4">Explicit Binding</Typography>
+
+      <Code noHeader>
+        <>{`
+function ask(question) {
+  console.log(this.teacher, question);
+}
+
+var workshop1 = {
+  teacher: "Marc",
+};
+
+var workshop2 = {
+  teacher: "Suzy",
+};
+
+ask.call(workshop1, "Can I explicitly set context?");
+// Marc Can I explicitly set context?
+
+ask.call(workshop2, "Can I explicitly set context?");
+// Suzy Can I explicitly set context?`}</>
+      </Code>
+      <Typography variant="h4">Hard Binding</Typography>
+      <Code noHeader>
+        <>{`
+var workshop = {
+  teacher: "Marc",
+  ask(question) {
+    console.log(this.teacher, question);
+  },
+};
+
+setTimeout(workshop.ask, 10, "Lost this?");
+// undefined Lost this?
+
+setTimeout(workshop.ask.bind(workshop), 10, "Hard bound this?");
+// Marc,Hard bound this?`}</>
+      </Code>
+      <Typography variant="h4">The new Keyword</Typography>
+      <Typography>
+        The purpose of the new keyword is actually to invoke a function with a
+        this keyword pointing at a whole new empty object. If we have invoking
+        functions and pointing them at a context object, like a workshop.ask,
+        that&aps;s one way. Or we say I&aps;m gonna invoke a function and give
+        it a specific object with .call or .apply or I&aps;m gonna force it with
+        a .bind.
+      </Typography>
+      <Code noHeader>
+        <>{`
+function ask(question) {
+  console.log(this.teacher, question);
+}
+
+var newEmptyObject = new ask("What is 'new' doing here?");
+// undefined What is 'new' ddoing here?`}</>
+      </Code>
+      <Typography>What the new keyword does?</Typography>
+      <ol className="grid list-[type1] gap-y-2 pl-9">
+        <li>
+          <Typography>Create a brand new empty object</Typography>
+        </li>
+        <li>
+          <Typography>* Link that object to another object</Typography>
+        </li>
+        <li>
+          <Typography>Call function with this set to the new object</Typography>
+        </li>
+        <li>
+          <Typography>
+            If function does not return an object, assume return of this
+          </Typography>
+        </li>
+      </ol>
+      <Typography variant="h4">Default Binding</Typography>
+      <Code noHeader>
+        <>{`
+var teacher = "Marc";
+
+function ask(question) {
+  console.log(this.teacher, question);
+}
+
+function askAgain(question) {
+  "use strict";
+  console.log(this.teacher, question);
+}
+
+ask("What's the non-strict-mode default?");
+// Marc What's the non-strict-mode default?
+
+
+askAgain("What's the strict-mode default?");
+// TypeError
+// we are getting an error because this is undefined, we need to use .call .apply or .bind to send the this`}</>
+      </Code>
+      <Typography variant="h4">Binding Precedence</Typography>
+      <Code noHeader>
+        <>{`
+var workshop = {
+  teacher: "Marc",
+  ask: function ask(question) {
+    console.log(this.teacher, question);
+  },
+};
+
+new (workshop.ask.bind(workshop))("What does this do?");
+// undefined What does this do?`}</>
+      </Code>
+
+      <ol className="grid list-[type1] gap-y-2 pl-9">
+        <li>
+          <Typography>Is this function called by new?</Typography>
+        </li>
+        <li>
+          <Typography>Is the function called by call() or apply()?</Typography>
+          <ol className="grid gap-y-2 pl-9">
+            <li>
+              <Typography>Note: bind() effectively uses apply()</Typography>
+            </li>
+          </ol>
+        </li>
+        <li>
+          <Typography>Is the function called on a context object?</Typography>
+        </li>
+        <li>
+          <Typography>DEFAULT: global object (except strict mode)</Typography>
+        </li>
+      </ol>
+      <Typography variant="h4">Arrow Functions & Lexical this</Typography>
+    </>
+  );
 };
 
 export default JavascriptBasicsObjectsPage;
